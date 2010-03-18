6491a279e026571548b2017fe65ab7f7d75244dd
Joseph Walton <joe@kafsemo.org>
2010-03-18 13:52:21 +0800

    Include the latest release date on the generated web page.

--- gen-index.pl
+++ gen-index.pl
@@ -14,18 +14,31 @@ use FindBin;
 use File::Spec;
 use HTML::Entities;
 
+my @months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+my %m2n;
+@m2n{@months} = (1 .. scalar(@months));
+
 my $index = new IO::File(File::Spec->catfile($FindBin::Bin, 'index.html'), '<') or die "Unable to open index.html: $!";
 
 my $htmlChanges = '';
 my $latest;
+my $latestDate;
 
 my $changes = new IO::File(File::Spec->catfile($FindBin::Bin, 'XML', 'Changes'), '<') or die "Unable to open Changes: $!";
 
 while (<$changes>) {
+	my $datestr;
+
+	if (my ($m, $d, $y) = /^[0-9].*([A-Z][a-z]{2})\s+(\d+).*\s([0-9]{4})\s/) {
+		my $mn = $m2n{$m};
+		$datestr = sprintf('%04d-%02d-%02d', $y, $mn, $d);
+	}
+
 	if (my ($vs) = /^(\d+\.\d+(?:\.\d+)?)\s+/) {
 		$vs = new version($vs);
 		if (!$latest || ($vs > $latest)) {
 			$latest = $vs;
+			$latestDate = $datestr;
 		}
 	}
 
@@ -64,6 +77,11 @@ EOP
 		if ($latest) {
 			print "${b}Release $latest${a}\n";
 		}
+	} elsif (my ($bef, $d, $aft) = /^(<p>Last modified: )(\d+-\d+-\d+)(<\/p>)/) {
+		if ($latestDate gt $d) {
+			$d = $latestDate;
+		}
+		print $bef, $d, $aft, "\n";
 	} else {
 		print $_;
 	}
