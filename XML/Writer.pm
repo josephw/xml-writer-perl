07a9af1351afeb0f26ba5350dcaffdcd9d6f7cd0
Joseph Walton <joe@kafsemo.org>
2012-01-07 23:55:40 +1100

    Remove redundant ref check.
    At this point $output should already be a reference.

--- XML/Writer.pm
+++ XML/Writer.pm
@@ -470,7 +470,7 @@ sub new {
                                 # If there is no OUTPUT parameter,
                                 # use standard output
       $output = $newOutput || \*STDOUT;
-      if ($outputEncoding && ref($output) && $output->isa('IO::Handle')) {
+      if ($outputEncoding && $output->isa('IO::Handle')) {
         if (lc($outputEncoding) eq 'utf-8') {
           binmode($output, ':encoding(utf-8)');
         } elsif (lc($outputEncoding) eq 'us-ascii') {
